8d1ec3c979dc803c6a7ce7a06fbdca7311633058
Imran Rafique <comunit76@gmail.com>
2019-02-19 21:05:58 +0000

    initial commit

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: process.env.APPLCATION_ID || "Your unsplash key",
+  SECRET: process.env.SECRET || "Your unsplash secret",
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000"
+};
